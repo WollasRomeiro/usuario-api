7f6272f0755a3bf531150bfdd6e02f5d6672f86d
WollasRomeiro <wollasromeiro@icloud.com>
2022-02-03 15:57:53 -0300

    finalizando crud

--- src/controller/usuariocontroller.js
+++ src/controller/usuariocontroller.js
@@ -1,9 +1,46 @@
+const { update } = require('../model/usuario');
 const usuario = require('../model/usuario');
 
 module.exports = {
     async index(req,res){
-        console.log('============')
         const usuarios = await usuario.find();
         res.json(usuarios)
-    }
-}
+    },
+
+    async detail(req,res){
+        const {_id} = req.params
+        console.log(_id)
+        const usuarios = await usuario.findOne({_id: _id});
+        res.json(usuarios)
+    },    
+    
+    async store(req,res){   
+            const {nome, senha} = req.body;
+
+            let datacreate = {}
+
+            datacreate = {
+                nome, senha
+            }
+            const usuarios = await usuario.create(datacreate);
+            res.json(usuarios)
+    },
+
+    async delete(req,res){
+        const {_id} = req.params
+        const usuarios = await usuario.findByIdAndDelete({_id});
+        res.json(usuarios)
+    },
+
+    async update(req,res){   
+        const { _id, nome, senha } = req.body;
+
+        let dataCreate = {}
+
+        dataCreate = {
+           nome, senha
+        }
+        const usuarios = await usuario.findByIdAndUpdate({_id}, dataCreate, {new:true});
+        res.json(usuarios)
+     },
+}   
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { update } = require('./controller/usuariocontroller');
 const usuariocontroller = require('./controller/usuariocontroller');
 
 const routes = express.Router();
@@ -8,5 +9,10 @@ routes.get('/', function(req,res){
 })
 
 routes.get('/usuario', usuariocontroller.index)
+routes.get('/usuario/:_id', usuariocontroller.detail)
+routes.post('/usuario', usuariocontroller.store)
+routes.delete('/usuario/:_id', usuariocontroller.delete)
+routes.put('/usuario', usuariocontroller.update)
+
 
 module.exports = routes;
